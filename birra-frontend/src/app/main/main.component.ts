68d57e0b382a0db10ae565ea621e393d5cb5a4be
jmeza <jhonatan.meza01@gmail.com>
2022-11-20 13:22:45 +0100

    styles view main

--- birra-frontend/src/app/main/main.component.ts
+++ birra-frontend/src/app/main/main.component.ts
@@ -1,6 +1,8 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { debounceTime, tap } from 'rxjs';
 import { BevandaService } from '../services/bevanda.service';
 
 @Component({
@@ -10,18 +12,31 @@ import { BevandaService } from '../services/bevanda.service';
 })
 export class MainComponent implements OnInit {
   page = 1;
-  length = 25;
+  length = 20;
   searchControl = new FormControl();
   birres: any[] = [];
+  service = false;
   constructor(
-    private _bevandaService: BevandaService
+    private _bevandaService: BevandaService,
+    private _router: Router
   ) { }
 
   ngOnInit(): void {
     this.loadBirres();
+
+    this.searchControl.valueChanges.pipe(
+      debounceTime(1000),
+      tap(() => this.page = 1)
+    ).subscribe(() => {
+      this.loadBirres();
+    });
   }
 
   loadBirres(): void {
+
+    if (this.service) return;
+    this.service = true;
+
     const body = {
       page: this.page,
       length: this.length,
@@ -29,21 +44,29 @@ export class MainComponent implements OnInit {
     };
 
     this._bevandaService.birraList(body).subscribe((list: any[]) => {
-      console.log(list);
       this.birres = list;
-    }, err => {
+      this.service = false;
+    }, (err: HttpErrorResponse) => {
       console.log(err);
+      this.service = false;
     });
   }
 
   prevPage(): void {
+    if (this.service) return;
     this.page--;
     this.loadBirres();
   }
 
   nextPage(): void {
+    if (this.service) return;
     this.page++;
     this.loadBirres();
+  }
+
+  logout(): void {
+    localStorage.removeItem('token');
+    this._router.navigate(['']);
   }
 
 }
